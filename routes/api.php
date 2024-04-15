7a1240832205de275f81ef54d572e7d726b5b714
Jhonner60 <jhonner.60@gmail.com>
2024-04-15 15:29:36 -0500

    video 3 migraciones y crud usuarios

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PermisoController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,4 +21,11 @@ Route::prefix("v1/auth")->group(function(){
         Route::get('profile', [AuthController::class, "funPerfil"]);
         Route::post('logout', [AuthController::class, "funSalir"]);    
     });
+});
+
+Route::middleware('auth:sanctum')->group(function(){
+    /* direcciones tabla Users, permisos y roles */
+    Route::apiResource("users", UserController::class);
+    Route::apiResource("permiso", PermisoController::class);
+    Route::apiResource("role", RoleController::class);
 });
